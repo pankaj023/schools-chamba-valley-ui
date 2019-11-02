4e019aedda0b8004bb5f263ec3cb2d687af1990f
pankaj023 <pankajmehta023@gmail.com>
2019-11-02 22:01:53 +0530

    added env from process

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/schools-chamba-valley'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 3000);
 
 //PATH LOCATION STARTEGY
 
 app.get('/*', function(req,res){
-  const fullPath = path.join(__dirname + '/dist/schools-chamba-valley/index.html');
+  const fullPath = path.join(__dirname + '/dist/index.html');
   console.log(" Fetching from.." + fullPath);
 	res.sendFile(fullPath);
 })
